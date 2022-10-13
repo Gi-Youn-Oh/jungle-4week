a9b00bdc299748a6d906ed1e54683598d33863eb
Gi-Youn-Oh <109953972+Gi-Youn-Oh@users.noreply.github.com>
2022-10-14 01:03:05 +0900

    number2

---
+++ Giyoun/Basic/효율적인 화폐구성.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m+1):
+        if d[j-array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+if d[m] == 10001: #최종적으로 m 원을 만드는 방법이 없을 경우
+    print(-1)
+else :
+    print(d[m])
